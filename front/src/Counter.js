84849cf9ca16e1015742a371edda920b9de101f0
dev.c.jade <dev.c.jade@gmail.com>
2022-06-27 15:48:20 +0900

    fist commit

---
+++ front/src/Counter.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    //state 사용하기 위해서는 사용해야함
+
+    super(props); // 부모 생성자 함수 호출
+
+    // state 초기화
+    this.state = {
+      number: 0,
+    };
+  }
+
+  render() {
+    const { number } = this.state;
+    return (
+      <div>
+        <h1>{number}</h1>
+        <button
+          onClick={() => {
+            this.setState({ number: number + 1 });
+          }}
+        >
+          +1
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
