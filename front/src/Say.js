aa366e8c8569bd32004e15df6b9e403d154f0324
dev.c.jade <dev.c.jade@gmail.com>
2022-06-27 20:16:44 +0900

    useState 함수는 여러번 사용가능

--- front/src/Say.js
+++ front/src/Say.js
@@ -3,14 +3,26 @@ import React, { useState } from 'react';
 const Say = () => {
   //배열을 요소를 구조분해할 이름은 마음대로 지어도됨
   const [message, setMessage] = useState('');
-  //useState 인자= 초기값. 리턴값 = 배열[초기값,세터함수]. 첫번째 파라미터 = 현재상태, 두번째파라미터 =  상태를 바꾸어 주는 함수
+  //useState 인자= 초기값. 리턴값 = 배열[초기값,세터함수]. 첫번째 파라미터 = 현재값(상태), 두번째파라미터 =  상태를 바꾸어 주는 함수
   const onClickEnter = () => setMessage('안녕하세요!');
   const onClickLeave = () => setMessage('안녕히 가세요!');
+
+  const [color, setColor] = useState('black');
+
   return (
     <div>
       <button onClick={onClickEnter}>입장</button>
       <button onClick={onClickLeave}>퇴장</button>
-      <h1>{message}</h1>
+      <h1 style={{ color }}>{message}</h1>
+      <button style={{ color: 'red' }} onClick={() => setColor('red')}>
+        빨간색
+      </button>
+      <button style={{ color: 'green' }} onClick={() => setColor('green')}>
+        초록색
+      </button>
+      <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
+        파란색
+      </button>
     </div>
   );
 };
