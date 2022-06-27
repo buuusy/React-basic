7a75b6dd8bf2233ff970974ddb55ab81524594ac
dev.c.jade <dev.c.jade@gmail.com>
2022-06-27 16:14:57 +0900

    con 생략하고 state 초기값 설정

--- front/src/Counter.js
+++ front/src/Counter.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  constructor(props) {
-    super(props); // 부모 생성자 함수 호출
-
-    // state 초기화
-    this.state = {
-      number: 0,
-      fixedNumber: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
 
   render() {
     const { number, fixedNumber } = this.state;
