8929587902a930945357f80aaa1082efd4118319
dev.c.jade <dev.c.jade@gmail.com>
2022-06-27 16:07:53 +0900

    ..

--- front/src/Counter.js
+++ front/src/Counter.js
@@ -7,14 +7,16 @@ class Counter extends Component {
     // state 초기화
     this.state = {
       number: 0,
+      fixedNumber: 0,
     };
   }
 
   render() {
-    const { number } = this.state;
+    const { number, fixedNumber } = this.state;
     return (
       <div>
         <h1>{number}</h1>
+        <h2> 바뀌지 않는 값: {fixedNumber}</h2>
         <button
           onClick={() => {
             this.setState({ number: number + 1 });
