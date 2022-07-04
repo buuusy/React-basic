88542fdf68b85789679f1656d4cdbcb8a97fa11f
buuusy <dev.c.jade@gmail.com>
2022-07-04 11:21:33 +0900

    에러처리

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
-import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoudary from './ErrorBoudary';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = { color: '#000000' };
+
+  handleClick = () => {
+    this.setState({ color: getRandomColor() });
+  };
+
   render() {
-    return <IterationSample />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤색상</button>
+        <ErrorBoudary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoudary>
+      </div>
+    );
   }
 }
 
